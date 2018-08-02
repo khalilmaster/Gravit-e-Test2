5a971bfca841ebee9b144d0337d66a8792eade2d
khalilmaster <khalilmaster@hotmail.fr>
2018-08-01 17:34:26 -0700

    2) In either C#, PHP or JavaScript write a program which compares two arrays and prints the values which are in one but not both arrays. For example given [1,2,3,4,5] and [1,2,3,0,5] the program would print “4” and “0”.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace gravit_e_test_2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int[] numbersA = { 1, 2, 3, 4, 5 };
+            int[] numbersB = { 1, 2, 3, 0, 5 };
+
+            IEnumerable<int> aOnlyNumbers = numbersA.Except(numbersB);
+            IEnumerable<int> bOnlyNumbers = numbersB.Except(numbersA);
+            Console.WriteLine("Array A:");
+            foreach (var a in aOnlyNumbers)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("Array B:");
+            foreach (var b in bOnlyNumbers)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
